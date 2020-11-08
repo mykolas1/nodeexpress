138d54a3514fcf3ea8c75fcced7f3442f54bc0dd
mykolas1 <mykolasba9@gmail.com>
2020-11-08 02:49:25 +0200

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
 const mysql = require('mysql');
 
-const express= require('express');
-var app = express();
 const bodyparser = require('body-parser');
 
-app.use(bodyparser.json);
+const express= require('express');
+const bodyParser= require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(3000, function() {
+    console.log('listening on 3000')
+  })
+
+  app.get('/employees', function (req, res) {
+    mySqlConnection.query('SELECT * FROM employee', (err, rows, fields)=> {
+        if(!err) {
+            console.log(rows);
+            res.send(rows);
+        }
+        else
+            console.log(err);
+    });
+  });
+
+  app.post('/employee', (req, res) => {
+    console.log('post');
+    mySqlConnection.query('INSERT INTO employee (Name, EmpCode, Salary) VALUES (?, ?, ?);', [req.body.name, req.body.empCode, req.body.salary], 
+      		function (err, results, fields) {
+      			if (err) throw err;
+			else console.log('Inserted ' + results.affectedRows + ' row(s).');
+	   	})
+    res.sendStatus(200);
+  });
+
+  app.post('/employee/update', (req, res) => {
+    console.log('update');
+    mySqlConnection.query('UPDATE employee SET Name = ?, EmpCode = ?, Salary = ? WHERE EmpID = ?', [req.body.name, req.body.empCode, req.body.salary, req.body.id], 
+			function (err, results, fields) {
+				if (err) throw err;
+				else console.log('Updated ' + results.affectedRows + ' row(s).');
+	});
+    res.sendStatus(200);
+  });
+
+  app.delete('/employee/:id/delete', (req, res) => {
+    console.log('delete');
+    mySqlConnection.query('DELETE FROM employee WHERE EmpID = ?', [req.params.id], 
+			function (err, results, fields) {
+				if (err) throw err;
+				else console.log('Deleted ' + results.affectedRows + ' row(s).');
+	});
+    res.sendStatus(200);
+  });
 
 var mySqlConnection = mysql.createConnection({
     host: "randomlion.mysql.database.azure.com",
@@ -16,23 +63,10 @@ var mySqlConnection = mysql.createConnection({
 });
 
 mySqlConnection.connect((err)=> {
-    if(!err)
+    if(!err) {
         console.log('LOGIN SUCCESS');
+    }
     else
         console.log('LOGIN ERROR: ' + JSON.stringify(err,undefined,2));
-})
+});
 
-app.listen(3000, () => {
-    console.log(`Example app listening at http://localhost:3000`)
-  })
-
-app.get('/e', (res, req)=> {
-    console.log('HIT');
-    res.send('HIT');
-    // mySqlConnection.query('SELECT * FROM employee', (err, rows, fields)=> {
-    //     if(!err)
-    //         console.log(rows);
-    //     else
-    //         console.log(err);
-    // });
-})
